f7f21dea4c92bc8602b04c1c06f2123f5a4e613e
angadd421 <31778115+angadd421@users.noreply.github.com>
2017-09-17 17:34:12 -0400

    Added successor function and solution function
    Added successor function and goal function to the input from data set.

--- problem1/route.py
+++ problem1/route.py
@@ -1,6 +1,10 @@
 # put your routing program here
+
 filename = "city-gps.txt"
 city_gps = []
+visited_states=[]
+goal_city = 'Jackman_Station,_Maine'
+
 with open(filename, 'r') as inputfile1:
     lines = [line.strip() for line in inputfile1]
     for line in lines:
@@ -26,3 +30,39 @@ with open(filename, 'r') as inputfile2:
             fourth = int(individualline1[3])
         fifth = individualline1[4]
         road_segments.append([first, second, third, fourth, fifth])
+
+
+def successor(s):
+    city_name=s
+    succ=[]
+    for r in range(0,len(road_segments)):      
+            if road_segments[r][0]==city_name:
+                succ.append(road_segments[r][1])
+                
+            elif road_segments[r][1] == city_name:
+                succ.append(road_segments[r][0])
+    return (succ)
+
+def solve_using_dfs(start_city):
+    fringe=[start_city]
+    
+    while len(fringe)>0:
+        popped_fringe=fringe.pop()
+        print("Popped Fringe is: ")
+        print(popped_fringe)
+        if popped_fringe not in visited_states:
+            visited_states.append(popped_fringe)
+            print ("Visited states are as follows: ")
+            print(visited_states)
+            for s in successor(popped_fringe):
+                if s == goal_city:
+                    return(s)
+                fringe.append(s)
+    return False
+    
+            
+    
+solution = solve_using_dfs('Abbot_Village,_Maine')
+print("You have reached: "+solution if solution else "Sorry, no solution is found :(")
+    
+    
