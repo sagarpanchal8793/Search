52e064465426b4de485d097ffe030e7c86e92c04
spanchal <spanchal@iu.edu>
2017-09-19 17:40:24 -0400

    Added bfs and dfs algorithm along with changes made to the file reading concept where the blank space and 0 values are taken care of

--- problem1/route.py
+++ problem1/route.py
@@ -1,9 +1,8 @@
-# put your routing program here
+###put your routing program here
 
 filename = "city-gps.txt"
 city_gps = []
-visited_states=[]
-goal_city = 'Jackman_Station,_Maine'
+
 
 with open(filename, 'r') as inputfile1:
     lines = [line.strip() for line in inputfile1]
@@ -23,46 +22,66 @@ with open(filename, 'r') as inputfile2:
         individualline1= line1.split(' ')
         first = individualline1[0]
         second = individualline1[1]
-        third = int(individualline1[2])
-        if individualline1[3] == '':
-            fourth = 0
+        if individualline1[2] == 0:
+            continue
         else:
-            fourth = int(individualline1[3])
+            third = int(individualline1[2])
+        if individualline1[3] == '' or individualline1[3] == 0:
+            continue
+        else:
+            fourth = int(individualline1[3])  
         fifth = individualline1[4]
         road_segments.append([first, second, third, fourth, fifth])
 
 
 def successor(s):
-    city_name=s
+    city_name=s[0]
     succ=[]
-    for r in range(0,len(road_segments)):      
-            if road_segments[r][0]==city_name:
-                succ.append(road_segments[r][1])
+    
+    for r in range(0,len(road_segments)):
+        if road_segments[r][0] == city_name:
+            succ.append([road_segments[r][1],s[1]+[city_name]])
                 
-            elif road_segments[r][1] == city_name:
-                succ.append(road_segments[r][0])
+        elif road_segments[r][1] == city_name:
+            succ.append([road_segments[r][0],s[1]+[city_name]])
+            
     return (succ)
 
-def solve_using_dfs(start_city):
-    fringe=[start_city]
+visited_states=[]
+
+def solve_using_bfs(initial_state):
+    fringe=[initial_state]
+    
+    while len(fringe)>0:
+        popped_fringe=fringe.pop(0)
+        if popped_fringe[0] not in visited_states:
+            visited_states.append(popped_fringe[0])
+            for s in successor(popped_fringe):
+                if len(s)>0:
+                    if s[0] == goal_city:
+                        return(s)
+                    fringe.append(s)
+    return False
+
+def solve_using_dfs(initial_state):
+    fringe=[initial_state]
     
     while len(fringe)>0:
         popped_fringe=fringe.pop()
-        print("Popped Fringe is: ")
-        print(popped_fringe)
-        if popped_fringe not in visited_states:
-            visited_states.append(popped_fringe)
-            print ("Visited states are as follows: ")
-            print(visited_states)
+        if popped_fringe[0] not in visited_states:
+            visited_states.append(popped_fringe[0])
             for s in successor(popped_fringe):
-                if s == goal_city:
-                    return(s)
-                fringe.append(s)
+                if len(s)>0:
+                    if s[0] == goal_city:
+                        return(s)
+                    fringe.append(s)
+                    
     return False
     
-            
-    
-solution = solve_using_dfs('Abbot_Village,_Maine')
-print("You have reached: "+solution if solution else "Sorry, no solution is found :(")
-    
-    
+
+goal_city = 'Fairborn,_Ohio'
+        
+solution = solve_using_bfs(['Abbot_Village,_Maine',[]])
+
+print "You have reached and path is : ",solution[1] if solution else "Sorry, no solution is found :("
+print "The count of cities is: ", len(solution[1])
