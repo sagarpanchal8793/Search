c056f3ed69d97e8dadd1c018b6b64c33e2533891
Ameya Angal <aangal@iu.edu>
2017-09-28 17:25:18 -0400

    Added linear conflict heuristic

--- problem3/solver16.py
+++ problem3/solver16.py
@@ -2,21 +2,29 @@
 from operator import itemgetter
 import copy
 import math
+
 # State: [[[],[],[],[]], [moves to get to that state], c(s),f(s)]
 
-# initial_state = [[[5,1,2,3],[0,9,6,4],[13,10,7,11],[14,15,12,8]],[], 0, 0] # 8 steps
-initial_state = [[[2,1,3,4],[5,6,7,8],[9,10,0,12],[13,14,11,15]],[], 0, 0] # not working apprx 9 steps
+# initial_state = [[[5,1,2,3],[0,9,6,4],[13,10,7,11],[14,15,12,8]],[], 0, 0] # 8 steps -- Working
+# initial_state = [[[5,1,2,3],[13,9,6,4],[0,10,7,11],[14,15,12,8]],[], 0, 0] # 9 steps -- Working
+# initial_state = [[[5,1,2,3],[13,9,6,4],[10,0,7,11],[14,15,12,8]],[], 0, 0] # 10 steps -- Working
+initial_state = [[[5,1,2,3],[13,9,6,4],[10,15,7,11],[14,0,12,8]],[], 0, 0] # 11 steps -- Working
+# initial_state = [[[5,1,2,3],[13,9,6,4],[10,15,7,11],[0,14,12,8]],[], 0, 0]
+# initial_state = [[[5,2,3,1],[13,9,6,4],[10,15,7,11],[14,12,0,8]],[], 0, 0] # 11+ steps -- Not Working
+# initial_state = [[[2, 1, 3, 4], [5, 6, 7, 8], [9, 10, 0, 12], [13, 14, 11, 15]], [], 0, 0]  # not working apprx 9 steps
+# initial_state = [[[2, 1, 3, 4], [5, 6, 7, 8], [9, 10, 0, 12], [13, 14, 11, 15]], [], 0, 0]
 # initial_state = [[[1,2,7,3],[5,10,6,4],[9,11,15,8],[13,14,0,12]],[], 0, 0]
 # initial_state = [[[1,3,6,4],[5,11,2,8],[0,9,7,12],[13,10,14,15]],[], 0, 0]
-goal_state = [[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]],[]]
+goal_state = [[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]], []]
 # fringe = [initial_state]
 # currentState = [[[1,2,3,4],[5,6,7,8],[9,10,0,12],[13,14,15,11]],[], 0, 9]
 goalStatePositions = {}
+lc = {}
 
 def CreateGoalStatePositionsDictionary(goal):
     for i, row in enumerate(goal):
         for j, col in enumerate(row):
-            goalStatePositions[goal[i][j]] = [i,j]
+            goalStatePositions[goal[i][j]] = [i, j]
 
 
 def GenerateParity(state):
@@ -25,22 +33,25 @@ def GenerateParity(state):
     j = 0
     flat_list = [item for element in state for item in element]
     for i in range(0, len(flat_list)):
-        for j in range(i+1, len(flat_list)):
+        for j in range(i + 1, len(flat_list)):
             if flat_list[j] == 0 or flat_list[i] == 0:
                 continue
-            if flat_list[j]<flat_list[i]:
+            if flat_list[j] < flat_list[i]:
                 parityInversion = parityInversion + 1
     return parityInversion
     # parityInversion = parityInversion + 1 for i in range(0, len(flat_list)) for j in range(i+1, len(flat_list)) if flat_list[j]<flat_list[i]
+
 
 def IsParityInversionSatisfied(currentstate, goal_state):
     goalParity = GenerateParity(goal_state)
     stateparity = GenerateParity(currentstate)
     return (goalParity % 2) == (stateparity % 2)
 
+
 #  returns a position of blank tile in the given puzzle
 def FindPositionOfBlankTile(puzzle):
     return [(row, col) for row in range(0, 4) for col in range(0, 4) if puzzle[row][col] == 0]
+
 
 # returns the manhattan distance for the puzzle state
 def ManhattanDistance(puzzle):
@@ -49,35 +60,79 @@ def ManhattanDistance(puzzle):
         for j, col in enumerate(row):
             if col == 0:
                 continue
-            manhattanDistance = manhattanDistance + abs(goalStatePositions[col][0] - i) + abs(goalStatePositions[col][1] - j)
-    return  manhattanDistance
+            manhattanDistance = manhattanDistance + abs(goalStatePositions[col][0] - i) + abs(
+                goalStatePositions[col][1] - j)
+    return manhattanDistance
 
-# return heuristic value MD/3
-def heuristicValue(state):
-    heuristic = int(math.ceil(ManhattanDistance(state)/3))
-    return heuristic
 
-lc = {}
-def conflictsInRow(state, tile, rownumber):
+
+
+# goalStatePositions1 = {3: [1, 0], 4: [1, 1], 5: [1, 2], 0: [2,2]}
+# goalStatePositions1 = {3: [0, 0], 4: [1, 0], 5: [2, 0]}
+
+def conflictsInRow(state, tile, tilerownumber, tilecolnumber):
     conflicts = 0
-    row = state[rownumber]
-    for i in range(0, len(row)-1):
-        tj = row[i]
-        tk = row[i+1]
-        if goalStatePositions[tj][0] == goalStatePositions[tk][0]:
-            if goalStatePositions[tj][1]>goalStatePositions[tk][1]:
-                conflicts +=1
-    return conflicts
+    row = state[tilerownumber]
+    conflictedTilesInRow = []
+    for k in range(0, len(row)):
+        tj = tile
+        tk = row[k]
+        if tj != 0 and tk != 0:
+            if goalStatePositions[tj][0] == goalStatePositions[tk][0]: # check if the 2 tiles are in same row in goal position
+                if (tilecolnumber > k and goalStatePositions[tj][1] < goalStatePositions[tk][1]) or (tilecolnumber < k and goalStatePositions[tj][1] > goalStatePositions[tk][1]):
+                    conflicts += 1
+                    conflictedTilesInRow.append(tk)
+    return conflicts, conflictedTilesInRow
 
+def conflictsInColumn(state, tile, tilerownumber, tilecolnumber):
+    conflicts = 0
+    conflictedTilesInColumn = []
+    column = [row[tilecolnumber] for row in state]
+    for k in range(0, len(column)):
+        tj = tile
+        tk = column[k]
+        if tj != 0 and tk != 0:
+            if goalStatePositions[tj][1] == goalStatePositions[tk][1]: # check if the 2 tiles are in same column in goal position
+                if (tilerownumber > k and goalStatePositions[tj][0] < goalStatePositions[tk][0]) or (tilerownumber < k and goalStatePositions[tj][0] > goalStatePositions[tk][0]):
+                    conflicts += 1
+                    conflictedTilesInColumn.append(tk)
+    return conflicts, conflictedTilesInColumn
 
 def linearConflict(state):
-    for i, row in state:
-        lc[i] = 0
-        for j, col in row:
-            # find C(state[i][j],i)
-            rowConflicts = conflictsInRow(state, state[i][j], i)
+    for i, row in enumerate(state):
+        lc[("row", i)] = 0
+        rConflicts = {}
+        for j, col in enumerate(row):
+            rConflicts[(state[i][j], i)] = conflictsInRow(state, state[i][j], i, j)
+        while max(rConflicts.iteritems(), key = lambda x: x[1][0])[1][0] > 0:
+            keytk = max(rConflicts.iteritems(), key = itemgetter(1))[0]
+            conflictedTilesWithtk = rConflicts[keytk][1]
+            rConflicts[keytk] = [0, conflictedTilesWithtk]
+            for tj in rConflicts[keytk][1]:
+                rConflicts[(tj, i)] = rConflicts[(tj, i)][0] - 1, rConflicts[(tj, i)][1]
+            lc[("row", i)] = lc[("row", i)] + 1
 
+    for k in range(0, len(state[0])):
+        lc[("col", k)] = 0
+        cConflicts = {}
+        for i, row in enumerate(state):
+            cConflicts[(state[i][k], k)] = conflictsInColumn(state, state[i][k], i, k)
+        while max(cConflicts.iteritems(), key=lambda x: x[1][0])[1][0] > 0:
+            keytk = max(cConflicts.iteritems(), key=itemgetter(1))[0]
+            conflictedTilesWithtk = cConflicts[keytk][1]
+            cConflicts[keytk] = [0, conflictedTilesWithtk]
+            for tj in cConflicts[keytk][1]:
+                cConflicts[(tj, k)] = cConflicts[(tj, k)][0] - 1, cConflicts[(tj, k)][1]
+            lc[("col", k)] = lc[("col", k)] + 1
 
+    return sum(values[1] for values in lc.items())
+
+# return heuristic value MD/3 plus linear conflicts
+def heuristicValue(state):
+    MD = int(math.ceil(float(ManhattanDistance(state)) / 3))
+    LC = (2 * linearConflict(state))
+    heuristic = MD + LC
+    return heuristic
 
 # # Gives the ceil value for misplacesTiles/3
 # def heuristicValue(state):
@@ -98,12 +153,13 @@ def MoveTileUp(puzzle, blankPosition, movesUntilNow, cost, succ):
     x = blankPosition[0][0]
     y = blankPosition[0][1]
     i = 0
-    for index in range(x+1, 4):
+    for index in range(x + 1, 4):
         movesUntilNowCopy = copy.deepcopy(movesUntilNow)
-        i = i+1
-        puzzleCopy[index][y], puzzleCopy[index-1][y] = puzzleCopy[index-1][y], puzzleCopy[index][y]
-        movesUntilNowCopy.append('U'+str(i)+str(y+1))
-        succ.append([copy.deepcopy(puzzleCopy), movesUntilNowCopy, cost+1, cost + heuristicValue(puzzleCopy)])
+        i = i + 1
+        puzzleCopy[index][y], puzzleCopy[index - 1][y] = puzzleCopy[index - 1][y], puzzleCopy[index][y]
+        movesUntilNowCopy.append('U' + str(i) + str(y + 1))
+        succ.append([copy.deepcopy(puzzleCopy), movesUntilNowCopy, cost + 1, cost + heuristicValue(puzzleCopy)])
+
 
 # returns a list of possible combinations for moving the tile down with list of move
 def MoveTileDown(puzzle, blankPosition, movesUntilNow, cost, succ):
@@ -111,12 +167,13 @@ def MoveTileDown(puzzle, blankPosition, movesUntilNow, cost, succ):
     x = blankPosition[0][0]
     y = blankPosition[0][1]
     i = 0
-    for index in range(x-1, -1, -1):
+    for index in range(x - 1, -1, -1):
         movesUntilNowCopy = copy.deepcopy(movesUntilNow)
-        i = i+1
-        puzzleCopy[index][y], puzzleCopy[index+1][y] = puzzleCopy[index+1][y], puzzleCopy[index][y]
-        movesUntilNowCopy.append('D'+str(i)+str(y+1))
+        i = i + 1
+        puzzleCopy[index][y], puzzleCopy[index + 1][y] = puzzleCopy[index + 1][y], puzzleCopy[index][y]
+        movesUntilNowCopy.append('D' + str(i) + str(y + 1))
         succ.append([copy.deepcopy(puzzleCopy), movesUntilNowCopy, cost + 1, cost + heuristicValue(puzzleCopy)])
+
 
 # return returns a list of possible combinations for moving the tile down with list of move
 def MoveTileRight(puzzle, blankPosition, movesUntilNow, cost, succ):
@@ -124,12 +181,13 @@ def MoveTileRight(puzzle, blankPosition, movesUntilNow, cost, succ):
     x = blankPosition[0][0]
     y = blankPosition[0][1]
     i = 0
-    for index in range(y-1, -1, -1):
+    for index in range(y - 1, -1, -1):
         movesUntilNowCopy = copy.deepcopy(movesUntilNow)
-        i = i+1
-        puzzleCopy[x][index], puzzleCopy[x][index+1] = puzzleCopy[x][index+1], puzzleCopy[x][index]
-        movesUntilNowCopy.append('R'+str(i)+str(x+1))
+        i = i + 1
+        puzzleCopy[x][index], puzzleCopy[x][index + 1] = puzzleCopy[x][index + 1], puzzleCopy[x][index]
+        movesUntilNowCopy.append('R' + str(i) + str(x + 1))
         succ.append([copy.deepcopy(puzzleCopy), movesUntilNowCopy, cost + 1, cost + heuristicValue(puzzleCopy)])
+
 
 # returns a list of possible combinations for moving the tile Up with list of move
 def MoveTileLeft(puzzle, blankPosition, movesUntilNow, cost, succ):
@@ -137,15 +195,15 @@ def MoveTileLeft(puzzle, blankPosition, movesUntilNow, cost, succ):
     x = blankPosition[0][0]
     y = blankPosition[0][1]
     i = 0
-    for index in range(y+1, 4):
+    for index in range(y + 1, 4):
         movesUntilNowCopy = copy.deepcopy(movesUntilNow)
-        i = i+1
-        puzzleCopy[x][index], puzzleCopy[x][index-1] = puzzleCopy[x][index-1], puzzleCopy[x][index]
-        movesUntilNowCopy.append('L'+str(i)+str(x+1))
+        i = i + 1
+        puzzleCopy[x][index], puzzleCopy[x][index - 1] = puzzleCopy[x][index - 1], puzzleCopy[x][index]
+        movesUntilNowCopy.append('L' + str(i) + str(x + 1))
         succ.append([copy.deepcopy(puzzleCopy), movesUntilNowCopy, cost + 1, cost + heuristicValue(puzzleCopy)])
 
 
-#  returns a list of successors, which individually contains two list.
+# returns a list of successors, which individually contains two list.
 def successor(currentState):
     succ = []
     blankPosition = FindPositionOfBlankTile(currentState[0])
@@ -154,6 +212,7 @@ def successor(currentState):
     MoveTileRight(currentState[0], blankPosition, currentState[1], currentState[2], succ)
     MoveTileLeft(currentState[0], blankPosition, currentState[1], currentState[2], succ)
     return succ
+
 
 # returns true if current state is present in the fringe and also removes the larger cost state
 def IsStateWithLargerTotalCostInFringe(current, fringe):
@@ -165,15 +224,16 @@ def IsStateWithLargerTotalCostInFringe(current, fringe):
                 fringe.remove(item)
                 return True
 
+
 # Solve using A* algorithm 3
 def solve(initial_state):
     # if IsParityInversionSatisfied(initial_state[0], goal_state[0]):
-    if  initial_state[0] == goal_state[0]:
+    if initial_state[0] == goal_state[0]:
         return initial_state
     fringe = [initial_state]
     closed = []
-    while len(fringe)>0:
-        fringe = sorted(fringe, key = itemgetter(3))
+    while len(fringe) > 0:
+        fringe = sorted(fringe, key=itemgetter(3))
         popped_fringe = fringe.pop(0)
         closed.append(popped_fringe)
         if popped_fringe[0] == goal_state[0]:
@@ -184,12 +244,14 @@ def solve(initial_state):
                     continue
                 if s in fringe:
                     # find if s is in fringe with larger value of f(s)
-                    if(IsStateWithLargerTotalCostInFringe(s, fringe)):
+                    if (IsStateWithLargerTotalCostInFringe(s, fringe)):
                         fringe.append(s)
                 else:
                     fringe.append(s)
     return False
     # return False
+
+
 # print "return IsStateWithLargerTotalCostInFringe is {0}".format(IsStateWithLargerTotalCostInFringe(currentState, fringe))
 # print "fringe is {0}".format(fringe)
 
@@ -204,8 +266,14 @@ if  solution == False:
 else:
     print "solution reached in {0} steps".format(solution[2])
     print "solution reached in {0} moves".format(solution[1])
-
-
+#
+# print "Conflicts in row are: {0}".format(conflictsInRow([[5, 4, 3]], 3, 0, 2))
+# print "Linear conflict in row are : {0}".format(linearConflict([[5, 0, 3]]))
+# print "Conflicts in column are: {0}".format(conflictsInColumn([[5],[4], [3]], 5, 0, 0))
+# print "Linear conflict in row are : {0}".format(linearConflict([[5],[4],[3]]))
+# print "Heuristic value for state : {0}".format(heuristicValue([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]))
+# print "Linear conflict for state is : {0}".format(linearConflict([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 0, 15, 14]]))
+# print "heuristic for state is : {0}".format(heuristicValue([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 0, 15, 14]]))
 # # print "dictionary is {0}".format(goalStatePositions)
 # print "MD for initial state is {0}".format(ManhattanDistance([[1,3,6,4],[5,11,2,8],[0,9,7,12],[13,10,14,15]]))
 # print "Misplaced tile heuristic for initial state is {0}".format(heuristicValue([[1,3,6,4],[5,11,2,8],[0,9,7,12],[13,10,14,15]]))
